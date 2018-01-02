e38e0ede7951078f9e4cafe77147723fc1fab815
zsh_o <965866221@qq.com>
2018-01-02 09:45:02 +0800

    rename

--- complexnm/bn.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Time      : 2017/12/29 16:27
-# Author    : zsh_o
-
---
+++ complexnm/normalization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Time      : 2017/12/29 16:27
+# Author    : zsh_o
+
+import numpy as np
+from keras.layers import Layer, InputSpec
+from keras import initializers, regularizers, constraints
+import keras.backend as K
+
